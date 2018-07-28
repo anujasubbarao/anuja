b0fa0831294842a2a0baaf98c434ef744a698213
anujamandalapu <41781814+anujasubbarao@users.noreply.github.com>
2018-07-28 21:02:22 +0530

    Update program5.py

--- program5.py
+++ program5.py
@@ -1,9 +1,11 @@
 n=int(raw_input())
- 
-if a>b and a>c:
-    print(a, " is largest")
-elif b>a and b>c:
-    print(b, " is largest")
+
+if (n1 > n2) and (n1 > n3):
+       biggest = num1
+elif (number2 > number1) and (n2 > n3):
+       biggest = n2
 else:
-    print(c, " is largest")
+       biggest = n3
+
+       print("The biggest number between",n1,",",n2,"and",n3,"is",biggest)
 
