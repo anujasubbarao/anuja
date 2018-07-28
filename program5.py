317e8e54e62b645df3e4e2d10f4c8ce44b409fbb
anujamandalapu <41781814+anujasubbarao@users.noreply.github.com>
2018-07-28 20:36:33 +0530

    Create program5.py

---
+++ program5.py
@@ -0,0 +1,9 @@
+n=int(raw_input())
+if(n >= n1) and n >= n2 :
+	     largest = n
+if (n1 >=n) and n1 >= n2 :
+	     largest = n1
+else:
+	     largest = n2
+	     print("the largest number between", "n", "n1","n2" is largest)
+
