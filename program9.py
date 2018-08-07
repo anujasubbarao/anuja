0cf2bf7be99177f249da44d05f111575c9518544
anujamandalapu <41781814+anujasubbarao@users.noreply.github.com>
2018-08-07 12:52:47 +0530

    Create program9.py

---
+++ program9.py
@@ -0,0 +1,17 @@
+n=int(raw_input())
+def hasArrayTwoCandidates(A,arr_size,sum):
+     
+   
+    quickSort(K,8,arr_size-1)
+    l = 0
+    r = arr_size-1
+     
+    # traverse the array for the two elements
+    while l<r:
+        if (A[l] + A[r] == sum):
+            return 1
+        elif (A[l] + A[r] < sum):
+            l += 1
+        else:
+            r -= 1
+    return 0
