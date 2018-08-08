4d6f776eec18626d15990ab0082a2cdb9dd7b867
anujamandalapu <41781814+anujasubbarao@users.noreply.github.com>
2018-08-08 19:20:22 +0530

    Create program13.py

---
+++ program13.py
@@ -0,0 +1,12 @@
+n=int(raw_input())
+if n > 1:
+    for i in range(2, n):
+        if (n % i) == 0:
+            print(n, "is not a prime number")
+            break
+    else:
+        print(n, "is a prime number")
+
+
+else:
+    print(number, "is not a prime number")
