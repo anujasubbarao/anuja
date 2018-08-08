63fb2b44d67f2443004495a9483d34fe243777cd
anujamandalapu <41781814+anujasubbarao@users.noreply.github.com>
2018-08-08 15:33:40 +0530

    Update prpgram19.py

--- prpgram19.py
+++ prpgram19.py
@@ -1,11 +1,11 @@
 n=int(raw_input())
 
 factorial = 1
-if num < 0:
+if n < 0:
    print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
+elif n == 0:
    print("The factorial of 0 is 1")
 else:
-   for i in range(1,num + 1):
+   for i in range(1,n + 1):
        factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+   print("The factorial of",n,"is",factorial)
