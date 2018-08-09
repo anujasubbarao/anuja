21ba2674c6de9182c6ca653f6c78451ecbfd30ce
anujamandalapu <41781814+anujasubbarao@users.noreply.github.com>
2018-08-09 12:09:38 +0530

    Update program18.py

--- program18.py
+++ program18.py
@@ -2,7 +2,7 @@ n=int(raw_input())
 l = 100
 u = 100000
 
-for num in range(l, u + 1):
+for n in range(l, u + 1):
    order = len(str(n))
    sum = 0
    temp = n
@@ -11,5 +11,5 @@ for num in range(l, u + 1):
        sum += digit ** order
        temp //= 10
 
-   if num == sum:
-       print(num)
+   if n == sum:
+       print(n)
