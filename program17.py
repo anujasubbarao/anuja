d4ff79180c46867c09741770f8db271a03070e32
anujamandalapu <41781814+anujasubbarao@users.noreply.github.com>
2018-08-09 12:51:26 +0530

    Create program17.py

---
+++ program17.py
@@ -0,0 +1,17 @@
+n=int(raw_input())
+sum = 0
+times = 0
+temp = n
+while temp > 0:
+        times = times + 1
+        temp = temp // 10
+temp = n
+while temp > 0:
+        rem = temp %10
+        sum = sum+ (Rem ** times)
+        temp // =10
+if n== sum:
+        print("Yes")
+else:
+        print("No")
+
