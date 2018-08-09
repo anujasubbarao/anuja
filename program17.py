858d5ef463f13797de1ee25cd70b200f34c1126c
anujamandalapu <41781814+anujasubbarao@users.noreply.github.com>
2018-08-09 12:53:51 +0530

    Update program17.py

--- program17.py
+++ program17.py
@@ -4,12 +4,12 @@ times = 0
 temp = n
 while temp > 0:
         times = times + 1
-        temp = temp // 10
+        temp = temp 10
 temp = n
 while temp > 0:
         rem = temp %10
         sum = sum+ (Rem ** times)
-        temp // =10
+        temp =10
 if n== sum:
         print("Yes")
 else:
