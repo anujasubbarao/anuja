9ccd8dfbddec790cec5fe05dd70604d7a241950f
anujamandalapu <41781814+anujasubbarao@users.noreply.github.com>
2018-08-09 11:44:35 +0530

    Update program12.py

--- program12.py
+++ program12.py
@@ -2,10 +2,10 @@ n=int(raw_input())
 temp=n
 rev=0
 while(n>0):
-    dig=n%10
+    rem=n%10
     rev=rev*10+dig
     n=n//10
 if(temp==rev):
-    print("The number is a palindrome!")
+    print("The number is a palindrome")
 else:
-    print("The number isn't a palindrome!")
+    print("The number isn't a palindrome")
