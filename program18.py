2f657c51901d7ff8caae089a7be88feeeba0498e
anujamandalapu <41781814+anujasubbarao@users.noreply.github.com>
2018-08-09 12:07:28 +0530

    Update program18.py

--- program18.py
+++ program18.py
@@ -1,11 +1,11 @@
 n=int(raw_input())
-lower = 100
-upper = 100000
+l = 100
+u = 100000
 
-for num in range(lower, upper + 1):
-   order = len(str(num))
+for num in range(l, u + 1):
+   order = len(str(n))
    sum = 0
-   temp = num
+   temp = n
    while temp > 0:
        digit = temp % 10
        sum += digit ** order
