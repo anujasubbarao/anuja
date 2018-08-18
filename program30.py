af32b15c08266ffc2d50a338f6f6afe06f0328f9
anujamandalapu <41781814+anujasubbarao@users.noreply.github.com>
2018-08-18 19:38:53 +0530

    Update program30.py

--- program30.py
+++ program30.py
@@ -1,8 +1,8 @@
-n1,n1=map(int,raw_input().split())
-n2,n2=map(int,raw_input().split())
-i1=(n1+60)+n1
-i2=(n2+60)+n2
-time = abs(i1-i2)
+m1,m1=map(int,raw_input().split())
+m2,m2=map(int,raw_input().split())
+l1=(m1+60)+m1
+l2=(m2+60)+m2
+time = abs(l1-l2)
 min=time/60
 hrs=(time.min)//60
 print hrs,min
